0ea01b82b5a827c5e6a64a7fcf43c21daf6cdda7
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-09 22:00:41 -0700

    adds some styling

--- frontend/components/landing/Landing.jsx
+++ frontend/components/landing/Landing.jsx
@@ -7,18 +7,43 @@ class Landing extends React.Component {
   render(){
     return(
       <div>
-        <header className="splash-head">
-            <a href="#">Logo</a>
-            <nav>
-              <ul>
-                <li>SignUp</li>
-                <li>LogIn</li>
-                <li>Demo Login</li>
-              </ul>
-            </nav>
+        <header className="landing-head">
+          <div className="header-content">
+            <div className="logo" >
+              <a href="#"><h1>Mindfull Note</h1></a>
+            </div>
+              <nav className="nav">
+                <ul>
+                  <li className="signup-link">
+                  <a href="#">SignUp</a>
+                  </li>
+                  <li>
+                    <div className="login-btn">
+                    <a href="#">LogIn</a>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="demo-btn">
+                    <a href="#">Demo</a>
+                    </div>
+                  </li>
+                </ul>
+              </nav>
+          </div>
         </header>
-        <section>
-          this is the body
+        <section className="landing-body">
+          <div className="strip">
+            <h1>40% off Mindnote Premier</h1>
+          </div>
+          <div className="poster">
+            <div className="poster-text">
+              <h1>Your notes. Organized. Effortless.</h1>
+              <h4>Take notes anywhere. Find information faster. Share ideas with anyone. Meeting notes, web pages, projects, to-do lists—with Evernote as your note taking app, nothing falls through the cracks.</h4>
+            </div>
+            <div className="poster-img">
+              <img src="/assets/landing_page/homepage-hero-desktop.png" />
+            </div>
+          </div>
         </section>
         <footer>
            this is the footer
