34a72b4970bdedec03ca456bf89528dac0d6c183
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-12 17:52:31 -0700

    user can create and edit notes

--- frontend/components/notes/notes_navbar_container.js
+++ frontend/components/notes/notes_navbar_container.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logout()),
-  createNote: () => dispatch(createNote({ note: { title: '_', body: '_' } })),
+  createNote: () => dispatch(createNote({ note: { title: 'New note', body: '...' } })),
 });
 
 
