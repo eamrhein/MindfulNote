0ece205fba7be5321c53c595f69aff9380b14ceb
Eric Amrhein <eamrhein@protonmail.ch>
2019-09-18 19:48:15 -0700

    dropdowns

---
+++ frontend/components/notebooks/notebooks_index/dropdown.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+
+class Dropdown extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false,
+    };
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+
+    if (!this.dropdownMenu.contains(event.target)) {
+
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
+
+    }
+  }
+
+  render() {
+    const { user, notebook, deleteNotebook } = this.props;
+    let deleteButton = 'Main Notebook';
+    if (user.email !== notebook.title) {
+      deleteButton = (
+        <button onClick={() => deleteNotebook(notebook.id)} type="submit">
+          Delete
+        </button>
+      );
+    }
+    return (
+      <div>
+        <button onClick={this.showMenu}>
+          <i className="fas fa-ellipsis-v" />
+        </button>
+
+        {this.state.showMenu ? (
+          <div
+            className="notebook-menu"
+            ref={element => {
+              this.dropdownMenu = element;
+            }}
+          >
+            {deleteButton}
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+};
+
+export default Dropdown;
--- frontend/components/notebooks/notebooks_index/notebook_index_item.jsx
+++ frontend/components/notebooks/notebooks_index/notebook_index_item.jsx
@@ -1,27 +1,21 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { timeSince } from '../../../util/calculations';
+import DropDown from './dropdown';
 import { Link } from 'react-router-dom';
 
 const NotebookIndexItem = ({ notebook, deleteNotebook, user }) => {
-  let deleteButton = 'Main Notebook';
-  if (user.email !== notebook.title) {
-    deleteButton = (
-      <button
-        onClick={() => deleteNotebook(notebook.id)}
-        type="submit"
-      >
-        <i className="fas fa-trash" />
-      </button>
-    );
-  }
   return (
     <tr key={notebook.id}>
       <td><Link to={`/notebooks/${notebook.id}`}>{notebook.title}</Link></td>
       <td>{user.email}</td>
       <td>{timeSince(notebook.updatedAt)}</td>
       <td>
-        {deleteButton}
+        <DropDown
+          notebook={notebook}
+          user={user}
+          deleteNotebook={deleteNotebook}
+        />
       </td>
     </tr>
   );
